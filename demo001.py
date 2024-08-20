c4f9aa37ca459842dcbf6296cdb2badec0a8a3d0
shi_hl <shi_hl@winning.com.cn>
2024-08-20 18:22:34 +0800

    可监控指定人员，自动回复消息

--- demo001.py
+++ demo001.py
@@ -3,8 +3,9 @@ from wxauto import WeChat
 wx = WeChat()
 
 # 发送消息
-who = '文件传输助手'
-for i in range(3):
+who = '牛牛139558~2022'
+# who = '文件传输助手'
+for i in range(10):
     wx.SendMsg(f'wxauto测试{i + 1}', who)
 
 # 获取当前聊天页面（文件传输助手）消息，并自动保存聊天图片
